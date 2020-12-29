51bb1b999f9dfec8e774763cf77e6657f38db8f2
patraz <59452908+patraz@users.noreply.github.com>
2020-12-29 13:04:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const drinkName = document.querySelector("#drink")
 const drinkBtn = document.querySelector("#new-drink")
-const twitterBtn = document.querySelector("#twitter")
 const ingredients = document.querySelector("#ingredients")
 const list = document.querySelector("#list2")
 const list2 = document.querySelector("#list")
 const listElements = document.getElementsByTagName("li")
 const lists = document.querySelectorAll("ul")
-
-console.log(lists)
+const drink = document.getElementById("drink")
+const intro = document.getElementById("quote")
 
 console.log(listElements)
 
-// Get Drink    from API
+// Get Drink  & ingredients  from API
 async function getDrink() {
     const apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
     try {
@@ -23,35 +22,18 @@ async function getDrink() {
         console.log(data.drinks[0])
         const arrDrinkObj = Object.entries(drinkObj)
         for (thing in drinkObj) {
-            // console.log(`${thing} : ${drinkObj[thing]}`)
-            // console.log(drinkObj[thing])
             for (let i = 0; i<15; i++) {
-                if (thing === `strIngredient${[i]}` && drinkObj[thing]!== null) {
-                    // console.log(`${thing} : ${drinkObj[thing]}`)
-                     
+                if (thing === `strIngredient${[i]}` && drinkObj[thing]!== null) {                     
                     const ingr = document.createTextNode(`${drinkObj[thing]}`)
-
-
                     const li = document.createElement('li')
-
                     li.appendChild(ingr)
-                    
-                   //console.log(ingr)
-                    
-
                     list.appendChild(li)
                 } else if (thing ===`strMeasure${[i]}` && drinkObj[thing]!== null) {
-                    // console.log(drinkObj[thing])
                     const msr = document.createTextNode(`${drinkObj[thing]}`)
-
-                    // console.log(msr)
-
                     const li2 = document.createElement("li")
-
                     li2.appendChild(msr)
-
                     list2.appendChild(li2)
-                    //console.log(li2)
+                    
                 }
             }
         }
@@ -59,6 +41,8 @@ async function getDrink() {
         console.log('no drink', error);
     }
 }
+
+// delete old ingredients & measurments
 
 function deleteLiGetDrink() {
     for (var i = 0; i<2; i++) {
@@ -69,11 +53,12 @@ function deleteLiGetDrink() {
     }
     getDrink();
 }
-// on load
-getDrink();
+
 
 
 // Getting new drink by using the button
 
 drinkBtn.addEventListener('click', deleteLiGetDrink)
 
+// on load
+getDrink();
